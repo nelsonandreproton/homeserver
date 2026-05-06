c4a057383a016ec4fc1c015178fb8ac0ab832b9b
Nelson André <nelsonandre@proton.me>
2026-05-06 14:22:02 +0100

    feat: add ptsquawk service to compose and deploy

--- deploy.sh
+++ deploy.sh
@@ -49,13 +49,14 @@ _sync ../GarminBot
 _sync ../HetznerCheck
 _sync ../JMJ2027
 _sync ../liturgia-das-horas
+_sync ../PTSquawk
 
 echo "[2/3] Rebuilding and restarting all services..."
 docker compose up -d --build
 
 echo "[3/3] Waiting for containers to become healthy..."
 all_ok=true
-for svc in cncsearch cncsearch_caddy garminbot hetzner-monitor jmj2027 liturgia-bot; do
+for svc in cncsearch cncsearch_caddy garminbot hetzner-monitor jmj2027 liturgia-bot ptsquawk; do
   _wait_healthy "$svc" || all_ok=false
 done
 
